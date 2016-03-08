11daa4be92f749dc335b3d690aacbb77b4eb8f66
Ayodeji Remi-Omosowon <remiomosowon@gmail.com>
2016-03-08 11:50:37 +0000

    Refactoring for extended use
    * Script use has increased and will be used by a lot more people
      for a variety of csv inputs not too different from the one I've
      been using.
    * Changes:
      -> Accomodate for variable row lengths for the description data
         at the top of the csv file.
      -> Abstract out the excel writing library; I've realised I might
         have to use a different excel library for some specific users
         who insist on having the newer openxml Excel format (because
         they don't like the compatibility warnings that show up when
         they open the older Excel format generated by xlwt).
      -> Minor code clean up here and there.
    
    There will probably be further changes as the requirements change,
    but it definitely looks a lot better and easier to maintain.

---
+++ src/XLWTWriter.py
@@ -0,0 +1,28 @@
+from Writer import Writer
+from xlwt import easyxf, Workbook
+
+# Implementation specifics for xlwt
+class XLWTWriter(Writer):
+    def __init__(self, filename):
+        Writer.__init__(self, filename)
+        self.excel_doc = Workbook()
+
+    def new_sheet(self, sheet_name):
+        return self.excel_doc.add_sheet(sheet_name)
+
+    def write_cell(self, sheet_name, row_no, col_no, val, bold=False):
+        sheet = self.get_sheet(sheet_name)
+
+        if bold:
+            style_bold = easyxf('font:bold True')
+            sheet.write(row_no, col_no, val, style_bold) 
+        else:
+            sheet.write(row_no, col_no, val)
+
+    # Set a specific column's width across all sheets.
+    def set_column_width(self, col_no, width):
+        for sheet_name in self.sheets:
+            self.sheets[sheet_name].col(col_no).width = width
+
+    def save_to_file(self, filename):
+        self.excel_doc.save(filename) 
